9284b8241ba7d4a2001ae891a9d04b86da79ad67
Ferenc Koler <ferenckoler@outlook.com>
2024-12-04 15:00:13 +0100

    PHP Tags Printing Comments

---
+++ Section 2 - Data Types, Variables & Built-in Functions/01-php-tags-printing-comments/index.php
@@ -0,0 +1,52 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>
+        <?php
+        echo "PHP From Scratch";
+        ?>
+    </title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">
+                <?= "PHP From Scratch" ?>
+            </h1>
+        </div>
+    </header>
+
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-2xl font-semibold mb-4">Welcome</h2>
+
+            <?php
+            echo "<p>In this section, we will learn the fundamentals of the PHP language</p>";
+            ?>
+        </div>
+    </div>
+
+    <?php
+    echo "<br>";
+    print "Hello from print";
+    echo "<br>", "Value 1", " Value 2", " Value 3";
+    // single line comment
+
+    /* multiple line comment
+    Line 1
+    Line 2
+    Line 3
+    Line 4
+    */
+    ?>
+</body>
+
+</html> -->
+
+<?php
+echo "<p>Without closing tag</p>";
