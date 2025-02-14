16584ad863082409122d35d77c02665eb488103a
fkoler <ferenckoler@outlook.com>
2025-02-14 20:59:06 +0100

    Functions & Return Values

---
+++ Section 5 - Functions/01-functions-and-return-values/index.php
@@ -0,0 +1,32 @@
+<?php
+
+function sayHelloButthead()
+{
+    echo "Hello Butthead" . "<br>";
+};
+
+sayHelloButthead();
+
+function sayGoodbyeButthead()
+{
+    return "Goodbye Butthead" . "<br>";
+};
+
+$goodbyeButthead = sayGoodbyeButthead();
+
+echo $goodbyeButthead;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Functions and Return Values</title>
+</head>
+
+<body class="bg-black text-white"></body>
+
+</html>
