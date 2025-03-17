a1222bbfedaffcae62a1768e29a746c32e211112
fkoler <ferenckoler@outlook.com>
2025-03-17 23:19:05 +0100

    Connect With PDO

---
+++ Section 8 - Database Integration and PDO/07-connect-pdo/index.php
@@ -0,0 +1,39 @@
+<?php
+// DB Credentials
+$host = "localhost";
+$port = 3306;
+$dbName = "blog";
+$username = "frenk";
+$password = "123456";
+
+// Data Source Name
+$dsn = "mysql:host={$host};port={$port};dbname={$dbName};charset=utf8";
+
+try {
+    // Create PDO instance
+    $pdo = new PDO($dsn, $username, $password);
+
+    // Set PDO to throw exeptions on error
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Database Connected...";
+} catch (PDOException $e) {
+    // If there is an error, catch it here
+    echo "Connection Failed: " . $e->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connect With PDO</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-900 text-gray-100 font-sans"></body>
+
+</html>
