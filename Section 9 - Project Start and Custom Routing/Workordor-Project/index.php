ec90520af0be0a98f4e083e1a31ef89c0914e2e7
fkoler <ferenckoler@outlook.com>
2025-03-24 22:18:01 +0100

    Folder Setup

---
+++ Section 9 - Project Start and Custom Routing/Workordor-Project/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Welcome to Workordor";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Workordor</title>
+</head>
+
+<body class="bg-gray-900 text-white"></body>
+
+</html>
