97441c4854d2ba0cfed45cd9434372b2c467b142
fkoler <ferenckoler@outlook.com>
2025-03-28 21:28:27 +0100

    Query Method & Fetch Listings

--- Section 10 - Database Class, Fetch & Display Listings/Workordor-Project/public/index.php
+++ Section 10 - Database Class, Fetch & Display Listings/Workordor-Project/public/index.php
@@ -1,17 +1,18 @@
 <?php
 
 require "../helpers.php";
-require basePath("Database.php");
-$config = require basePath("config/db.php");
-
-$db = new Database($config);
 require basePath("Router.php");
+require basePath("Database.php");
 
+// 1. Instatiate the router
 $router = new Router();
 
+// 2. Get routes
 $routes = require basePath("routes.php");
 
+// 3. Get current URI and HTTP method
 $uri = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
 
+// 4. Route the request
 $router->route($uri, $method);
