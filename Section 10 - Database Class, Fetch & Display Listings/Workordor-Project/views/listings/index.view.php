5a511d98bd9d45cc14430b65fe099f2cf3b46c98
fkoler <ferenckoler@outlook.com>
2025-03-30 23:02:15 +0200

    Pass Data To View #3

--- Section 10 - Database Class, Fetch & Display Listings/Workordor-Project/views/listings/index.view.php
+++ Section 10 - Database Class, Fetch & Display Listings/Workordor-Project/views/listings/index.view.php
@@ -34,10 +34,6 @@
                 </div>
             <?php endforeach; ?>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
 </section>
 
 <?= loadPartial("bottom-banner") ?>
