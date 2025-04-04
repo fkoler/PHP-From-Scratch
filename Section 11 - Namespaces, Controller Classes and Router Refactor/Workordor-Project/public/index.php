0a1f6f26714fa182069db3cb90470633521b0a3e
fkoler <ferenckoler@outlook.com>
2025-04-04 17:07:01 +0200

    Folder Structure Refactor

---
+++ Section 11 - Namespaces, Controller Classes and Router Refactor/Workordor-Project/public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require "../helpers.php";
+require basePath("Framework/Router.php");
+require basePath("Framework/Database.php");
+
+// 1. Instatiate the router
+$router = new Router();
+
+// 2. Get routes
+$routes = require basePath("routes.php");
+
+// 3. Get current URI and HTTP method
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+// inspectAndDie($uri);
+$method = $_SERVER["REQUEST_METHOD"];
+
+// 4. Route the request
+$router->route($uri, $method);
