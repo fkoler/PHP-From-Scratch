5dc24c584654c37fe0c452440a5142e97665ff0d
fkoler <ferenckoler@outlook.com>
2025-04-13 22:14:50 +0200

    Start with the CRUD operations

---
+++ Section 12 - Create Update and Delete Listings/Workordor-Project/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+require "../helpers.php";
+
+use Framework\Router;
+
+// 1. Instatiate the router
+$router = new Router();
+
+// 2. Get routes
+$routes = require basePath("routes.php");
+
+// 3. Get current URI and HTTP method
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+// 4. Route the request
+$router->route($uri);
