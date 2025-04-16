502d637d8813898b619f0191117fb1beeda100b5
fkoler <ferenckoler@outlook.com>
2025-04-16 23:35:34 +0200

    Implement Validation

--- Section 12 - Create Update and Delete Listings/Workordor-Project/App/Controllers/ListingController.php
+++ Section 12 - Create Update and Delete Listings/Workordor-Project/App/Controllers/ListingController.php
@@ -63,6 +63,28 @@ class ListingController
 
         $newListingData = array_map("sanitize", $newListingData);
 
-        inspectAndDie($_POST);
+        $requiredFields = ["title", "description", "email", "city", "state"];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (
+                empty($newListingData[$field]) ||
+                !Validation::string($newListingData[$field])
+            ) {
+                $errors[$field] = ucfirst($field) . " is required";
+            }
+        }
+
+        if (!empty($errors)) {
+            // Reload views with errors
+            loadView("listings/create", [
+                "errors" => $errors,
+                "listing" => $newListingData,
+            ]);
+        } else {
+            // Submit data
+            echo "Success";
+        }
     }
 }
