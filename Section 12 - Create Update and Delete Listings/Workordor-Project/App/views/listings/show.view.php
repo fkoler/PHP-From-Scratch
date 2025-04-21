ba3af7741a7710e9d16e655daeee37093f1fe92d
fkoler <ferenckoler@outlook.com>
2025-04-21 17:59:11 +0200

    Flash Messages - Edit view created

--- Section 12 - Create Update and Delete Listings/Workordor-Project/App/views/listings/show.view.php
+++ Section 12 - Create Update and Delete Listings/Workordor-Project/App/views/listings/show.view.php
@@ -12,7 +12,7 @@
 
             <div class="flex space-x-4 ml-4">
                 <a
-                    href="/edit"
+                    href="/listings/edit/<?= $listing->id ?>"
                     class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
 
                 <!-- Delete Form -->
