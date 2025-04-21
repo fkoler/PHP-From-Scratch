54c578ff81142492a59e956976c1c0ab66e84663
fkoler <ferenckoler@outlook.com>
2025-04-21 22:23:21 +0200

    User Controller & Views - UserController

--- Section 13 - Authentication Authorization and Sessions/Workordor-Project/routes.php
+++ Section 13 - Authentication Authorization and Sessions/Workordor-Project/routes.php
@@ -11,3 +11,7 @@ $router->post("/listings", "ListingController@store");
 $router->put("/listings/{id}", "ListingController@update");
 
 $router->delete("/listings/{id}", "ListingController@destroy");
+
+// User
+$router->get("/auth/register", "UserController@create");
+$router->get("/auth/login", "UserController@login");
