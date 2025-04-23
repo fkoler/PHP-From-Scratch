a240ee383025242c827964edb4d75a1b6bab8f6d
fkoler <ferenckoler@outlook.com>
2025-04-23 23:21:52 +0200

    Session Class & Set User - setSession and getSession

--- Section 13 - Authentication Authorization and Sessions/Workordor-Project/Framework/Session.php
+++ Section 13 - Authentication Authorization and Sessions/Workordor-Project/Framework/Session.php
@@ -15,4 +15,28 @@ class Session
             session_start();
         }
     }
+
+    /**
+     * Set a session key/value pair
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public static function setSession($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Get a session value by the key
+     * 
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public static function getSession($key, $default = null)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
+    }
 }
