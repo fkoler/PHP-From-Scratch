b18dcbb89335314bced0deef1a1918e24046a8e5
fkoler <ferenckoler@outlook.com>
2025-04-25 21:44:46 +0200

    Delete Authorization - Order listings by creation date DESC on home and listings views

--- Section 13 - Authentication Authorization and Sessions/Workordor-Project/App/Controllers/ListingController.php
+++ Section 13 - Authentication Authorization and Sessions/Workordor-Project/App/Controllers/ListingController.php
@@ -23,7 +23,7 @@ class ListingController
      */
     public function index()
     {
-        $listings = $this->db->query("SELECT * FROM listings;")->fetchAll();
+        $listings = $this->db->query("SELECT * FROM listings ORDER BY created_at DESC;")->fetchAll();
 
         loadView("listings/index", [
             "listings" => $listings,
